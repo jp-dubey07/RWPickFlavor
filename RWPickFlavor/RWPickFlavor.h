aa6aec36505e4ef6b48ac6bb1ece6e3116b32acf
ASM Technologies <asmtechnologies@ASMs-MacBook-Pro.local>
2016-03-19 16:26:03 +0530

    Initial Commit

---
+++ RWPickFlavor/RWPickFlavor.h
@@ -0,0 +1,21 @@
+//
+//  RWPickFlavor.h
+//  RWPickFlavor
+//
+//  Created by ABC Technologies on 19/03/15.
+//  Copyright © 2015 OrganizationNameHere. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RWPickFlavor.
+FOUNDATION_EXPORT double RWPickFlavorVersionNumber;
+
+//! Project version string for RWPickFlavor.
+FOUNDATION_EXPORT const unsigned char RWPickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RWPickFlavor/PublicHeader.h>
+
+
+// pod 'PodName', '~> 1.0.0'
+// pod 'MyPodName', :path=>'/Path where pod is located - local location'
